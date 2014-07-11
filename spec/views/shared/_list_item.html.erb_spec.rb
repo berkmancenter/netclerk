6b5e13026d91e84a08ebb91a4a3e2cddc1e2ec7d
Ryan Westphal <ryan@trippingthebits.com>
2014-07-11 14:34:27 -0400

    can add classes to list_item

--- spec/views/shared/_list_item.html.erb_spec.rb
+++ spec/views/shared/_list_item.html.erb_spec.rb
@@ -4,13 +4,26 @@ describe ( 'shared/list_item' ) {
   subject { rendered }
 
   let ( :country ) { Country.first }
-  
-  before {
-    render partial: 'shared/list_item', object: country
+
+  context ( 'default' ) {
+    before {
+      render partial: 'shared/list_item', object: country
+    }
+
+    it { should have_css 'li' }
+
+    it {
+      # removed, part of country partial
+      should_not have_css 'li.media'
+    }
   }
 
-  it {
-    # removed, part of country partial
-    should_not have_css 'li.media'
+  context ( 'with extra class' ) {
+    before {
+      render partial: 'shared/list_item', object: country, locals: { classes: 'list-group-item' }
+    }
+
+    it { should have_css 'li.list-group-item' }
   }
+
 }
