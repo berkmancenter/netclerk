19d54a6d76a3d693e7782e96a9a13948f16b58a9
Ryan Westphal <ryan@trippingthebits.com>
2014-07-25 11:46:48 -0400

    #8860 - status titles

--- spec/views/pages/show.html.erb_spec.rb
+++ spec/views/pages/show.html.erb_spec.rb
@@ -27,25 +27,26 @@ describe ( 'pages/show' ) {
     }
 
     it { should have_css '.list-group.pages-status-success' }
+    it { should have_css '.list-group.pages-status-info' }
+    it { should have_css '.list-group.pages-status-warning' }
+    it { should have_css '.list-group.pages-status-danger' }
 
-    it {
-      should have_css '.pages-status-success .list-group-item-success'
-    }
+    it { should have_css '.pages-status-success .list-group-item-success', text: 'available' }
 
     it {
       should have_css '.pages-status-success a.list-group-item', text: 'United States', count: 1
     }
 
-    it { should have_css '.pages-status-info .list-group-item-info' }
+    it { should have_css '.pages-status-info .list-group-item-info', text: 'a bit different' }
 
-    it { should have_css '.pages-status-warning .list-group-item-warning' }
+    it { should have_css '.pages-status-warning .list-group-item-warning', text: 'very different' }
 
     it {
       # china's warning status isn't from today
       should_not have_css '.pages-status-warning a.list-group-item', text: 'China'
     }
 
-    it { should have_css '.pages-status-danger .list-group-item-danger' }
+    it { should have_css '.pages-status-danger .list-group-item-danger', text: 'not available' }
 
     it {
       should have_css '.pages-status-danger a.list-group-item', text: 'China', count: 1
