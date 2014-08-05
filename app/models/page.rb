28e7dbed563ff18da8b52e858bc4aadd987c0c8b
Ryan Westphal <ryan@trippingthebits.com>
2014-08-05 17:12:55 -0400

    #8735 - update page url on redirect, skip a day

--- app/models/page.rb
+++ app/models/page.rb
@@ -16,6 +16,12 @@ class Page < ActiveRecord::Base
         'Connection' => 'close',
         'User-Agent' => 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36',
       } ).read
+    rescue OpenURI::HTTPRedirect => e
+      # update our url and try again another day
+      puts "  #{url} => #{e.uri}"
+      self.url = e.uri.to_s
+      save
+      return
     rescue SocketError
       puts '  SocketError (consider removing from NetClerk)'
       return
