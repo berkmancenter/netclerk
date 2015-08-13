4b51c9db416096df59681d8336214012d7e1d950
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-08-13 14:47:59 -0400

    #12524 - full json api

--- app/controllers/jsonapi_controller.rb
+++ app/controllers/jsonapi_controller.rb
@@ -9,12 +9,19 @@ class JsonapiController < ApplicationController
           type: 'statuses',
           id: s.id.to_s,
           attributes: {
+            url: s.page.url,
+            country: s.country.iso2,
+            code: 200,
+            probability: s.value.to_f / 3.0,
+            available: s.value.to_f >= 2.0,
+            created: s.created_at
           }
         }
 
       }
     }
-
+      
+    response.headers[ 'Content-Type' ] = 'application/vnd.api+json'
     render json: api_data
   end
 end
